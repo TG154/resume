55644c69e0c17c02b13b957eb5d12189faba4ae2
TG154 <tg154isgreat@gmail.com>
2025-09-03 17:00:48 +0500

    fixes

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -3,37 +3,7 @@ import config from '../../../config.json';
 const sumfetch = async (args: string[]): Promise<string> => {
   if (config.ascii === 'cveinnt') {
     return `                                                  
-<<<<<<< HEAD
-████████╗ ██████╗  ██╗███████╗██╗  ██╗       sumfetch: summary display
-╚══██╔══╝██╔════╝ ███║██╔════╝██║  ██║      -----------
-   ██║   ██║  ███╗╚██║███████╗███████║       ABOUT
-   ██║   ██║   ██║ ██║╚════██║╚════██║       ${config.name}
-   ██║   ██║   ██║ ██║╚════██║╚════██║       ${config.ps1_hostname}
-   ╚═╝    ╚═════╝  ╚═╝╚══════╝     ╚═╝       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-                                            爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-                                            -----------
-                                             CONTACT 
-                                             <u><a href="mailto:${config.email_no_1}" target="_blank">${config.email_no_1}</a></u>
-                                             <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-                                             <u><a href="https://instagram.com/${config.social.instagram}" target="_blank">instagram.com/${config.social.instagram}</a></u>
-`;
-  } else {
-    return `
-████████╗ █████╗ ██╗     ██╗  ██╗ █████╗     sumfetch
-╚══██╔══╝██╔══██╗██║     ██║  ██║██╔══██╗   -----------
-   ██║   ███████║██║     ███████║███████║    ABOUT
-   ██║   ██╔══██║██║     ██╔══██║██╔══██║    ${config.name}
-   ██║   ██║  ██║███████╗██║  ██║██║  ██║    <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-                                            -----------
- ██████╗  ██████╗ ██╗  ██╗ █████╗ ██████╗    CONTACT 
-██╔════╝ ██╔═══██╗██║  ██║██╔══██╗██╔══██╗   <u><a href="mailto:${config.email_no_1}" target="_blank">${config.email_no_1}</a></u>
-██║  ███╗██║   ██║███████║███████║██████╔╝   <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-██║   ██║██║   ██║██╔══██║██╔══██║██╔══██╗
-╚██████╔╝╚██████╔╝██║  ██║██║  ██║██║  ██║ -----------
- ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝               
 
-=======
  :                                               sumfetch
  :             ===+++++===-                     -----------
  :          ==++**********++=-           %       ABOUT
